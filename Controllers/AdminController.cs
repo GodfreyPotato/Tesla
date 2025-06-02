93a7640d413cbb33c9a9d5d4c5cc3f315fdaeba0
GodfreyPotato <godfreyjavier88@gmail.com>
2025-06-03 00:03:03 +0800

    first upload, working create product and product listing with iamge

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using practiceQuiz.DataAccess;
+using System.Data;
+using tesla.Models;
+
+namespace tesla.Controllers
+{
+    public class AdminController : Controller
+    {
+        DatabaseHelper _helper;
+        public AdminController()
+        {
+            _helper = new DatabaseHelper();
+        }
+        public IActionResult ProductList()
+        {
+            ViewBag.img = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/productImages");
+            return View(getProducts());
+        }
+
+        private List<Product> getProducts()
+        {
+            List<Product> products = new List<Product>();
+
+            DataTable dt = _helper.read("Select * from products;");
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                products.Add(new Product
+                {
+                    id = int.Parse(dr["id"].ToString()),
+                    prod_name = dr["prod_name"].ToString(),
+                    prod_description = dr["prod_description"].ToString(),
+                    prod_img = dr["prod_img"].ToString(),
+                    price = decimal.Parse(dr["price"].ToString()),
+                    cat_id = int.Parse(dr["cat_id"].ToString())
+                });
+            }
+
+            return products;
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using practiceQuiz.DataAccess;
+using System.Data;
+using tesla.Models;
+
+namespace tesla.Controllers
+{
+    public class ProductController : Controller
+    {
+        DatabaseHelper _helper;
+        public ProductController()
+        {
+            _helper = new DatabaseHelper();
+        }
+
+        private List<Category> getCategories()
+        {
+            List<Category> categories = new List<Category>();
+
+            DataTable dt = _helper.read("Select * from categories;");
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                categories.Add(new Category
+                {
+                    id = int.Parse(dr["id"].ToString()),
+                    cat_name = dr["cat_name"].ToString(),
+                    cat_description = dr["cat_description"].ToString()
+                });
+            }
+
+            return categories;
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+           
+
+            ViewBag.Categories = getCategories();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.ImageFile != null && model.ImageFile.Length > 0)
+                {
+                    var fileName = Path.GetFileNameWithoutExtension(model.ImageFile.FileName);
+                    var extension = Path.GetExtension(model.ImageFile.FileName);
+                    var uniqueFileName = $"{fileName}_{Guid.NewGuid()}{extension}";
+                    var imagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/productImages", uniqueFileName);
+
+                    using (var stream = new FileStream(imagePath, FileMode.Create))
+                    {
+                        await model.ImageFile.CopyToAsync(stream);
+                    }
+
+                    model.prod_img = uniqueFileName; // Save filename in DB
+                }
+
+         
+                string query = $"INSERT INTO products (prod_name, prod_description, price, prod_img, cat_id) VALUES " +
+                               $"('{model.prod_name}', '{model.prod_description}', {model.price}, '{model.prod_img}', {model.cat_id})";
+                _helper.execute(query);
+
+                return RedirectToAction("ProductList","Admin");
+            }
+
+         
+            ViewBag.Categories = getCategories();
+            return View(model);
+        }
+    }
+}
