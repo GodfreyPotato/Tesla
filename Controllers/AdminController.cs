1e1344f3b4a89ef459d56c9fd5fbf2c880285cf3
Gabriel Lipaysa <gabriellipaysa@gmail.com>
2025-06-04 01:17:38 +0800

    Done sorting ng status order and OrderDetails

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -41,8 +41,14 @@ namespace tesla.Controllers
             return products;
         }
 
-        public IActionResult OrderList() {
-            return View(getOrders());
+        public IActionResult OrderList(string? status) {
+            var orders = getOrders();
+
+            if (!string.IsNullOrEmpty(status))
+            {
+                orders = orders.Where(order => order.status == status).ToList();
+            }
+            return View(orders);
         }
         public List<Order> getOrders() {
             List<Order> orders = new List<Order>();
@@ -57,12 +63,51 @@ namespace tesla.Controllers
                     user_id = int.Parse(dr["user_id"].ToString()),
                     address = dr["address"].ToString(),
                     totalAmount = decimal.Parse(dr["totalAmount"].ToString()),
-                    date = DateTime.Parse(dr["date"].ToString()).ToString("MMMM dd, yyyy", CultureInfo.InvariantCulture), //Bale sa adding or ng order dapat naayus na or smth
-                    //ToModify na lang to
+                    date = DateTime.Parse(dr["date"].ToString()).ToString("MMMM dd, yyyy", CultureInfo.InvariantCulture),
                     status = dr["status"].ToString()
                 });
             }
             return orders;
+        }
+
+
+        public IActionResult OrderDetails(int id)
+        {
+            return View(getOrderDetails(id));
+        }
+
+        public OrderDetail getOrderDetails(int orderId) {
+            OrderDetail orderDetail = new OrderDetail();
+            orderDetail.items = new List<OrderItem>();
+
+            DataTable dt = _helper.read(@"
+            SELECT 
+                orders.id AS 'OrderId',
+                orders.date AS 'OrderDate',
+                orders.totalAmount,
+                products.prod_name AS 'ProductName',
+                cartitems.quantity,
+                products.price,
+                (products.price * cartitems.quantity) AS 'Subtotal'
+            FROM orders
+            JOIN cartitems ON orders.cart_id = cartitems.cart_id
+            JOIN products ON cartitems.product_id = products.id
+            WHERE orders.id = " + orderId);
+
+            foreach(DataRow row in dt.Rows)
+            {
+                orderDetail.id = Convert.ToInt32(row["OrderId"]);
+                orderDetail.date = Convert.ToDateTime(row["OrderDate"]).ToString("MMMM dd, yyyy");
+                orderDetail.totalAmount = Convert.ToDecimal(row["totalAmount"]);
+
+                orderDetail.items.Add(new OrderItem
+                {
+                    productName = row["ProductName"].ToString(),
+                    quantity = Convert.ToInt32(row["quantity"]),
+                    price = Convert.ToDecimal(row["price"]),
+                });
+            }
+            return orderDetail;
         }
     }
 }
