7cbcd9bfe7d5494a45f60405927e891861b99086
GianEd36 <gcenteno_19ur0202@psu.edu.ph>
2025-06-04 16:58:56 +0800

    Cart w/quantity control & remove from cart functionality

--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -1,10 +1,13 @@
-﻿namespace tesla.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace tesla.Models
 {
     public class CartItem
     {
         public int id { get; set; }
         public int product_id { get; set; }
-        public int quanitity { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1")]
+        public int quantity { get; set; }//quanitity
         public string date { get; set; }
         public int cart_id { get; set; }
 
