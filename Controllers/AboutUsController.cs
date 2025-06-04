8afa6e520366a8d174c649e3df0b5d64328b4674
secretentropy <158293262+secretentropy@users.noreply.github.com>
2025-06-05 00:48:59 +0800

    About Us, FAQ added, Small UI Changes to Register and Login Pages

---
+++ Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tesla.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
