00531b3e202a78108a44c8137936ea7acca5156d
GodfreyPotato <godfreyjavier88@gmail.com>
2025-06-05 12:53:25 +0800

    updates

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using practiceQuiz.DataAccess;
+using System.Data;
+using tesla.Models;
+
+namespace tesla.Controllers
+{
+    public class ProfileController : Controller
+    {
+        DatabaseHelper _helper;
+        public ProfileController()
+        {
+            _helper = new DatabaseHelper();
+        }
+
+        public IActionResult ShowProfile()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("id")))
+            {
+
+            }
+            DataTable user = _helper.read($"select * from users where id = {HttpContext.Session.GetString("id")}");
+            var userModel = new User
+            {
+                firstname = user.Rows[0]["firstname"].ToString(),
+                middlename= user.Rows[0]["middlename"].ToString(),
+                lastname = user.Rows[0]["lastname"].ToString(),
+                address = user.Rows[0]["address"].ToString(),
+                email = user.Rows[0]["email"].ToString(),
+                password = user.Rows[0]["password"].ToString(),
+
+            };
+            
+            return View(userModel);
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace tesla.Models
+{
+    public class User
+    {
+        public string firstname { get; set; }
+        public string middlename { get; set; }
+        public string lastname { get; set; }
+        public string address { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
